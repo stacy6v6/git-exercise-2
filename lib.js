a2559b57aeda9f16cdd9991fdf3e2e3b19e62f39
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-05 08:47:08 +0800

    added whitespace at the end of brackets

--- lib.js
+++ lib.js
@@ -7,13 +7,11 @@ const rectangleArea = (a, b) => a * b;
 const isPalindrome = (str) => {
     str = str.toLowerCase();
 
-    for (let i = 0; i < str.length / 2; i++){
-        if (str[i] === str[str.length - 1 - i]){
-
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] === str[str.length - 1 - i]) {
             return true;
         
         } else {
-          
             return false;
         
         }
